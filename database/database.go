c67298b5e8e7befef6ccdca2bd2e6ea2613732aa
koybigino <koybigino@gmail.com>
2022-12-21 15:41:27 +0100

    edit the database.go file

--- database/database.go
+++ database/database.go
@@ -17,6 +17,22 @@ func Connection() *gorm.DB {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Some error occured. Err: %s", err)
+
+		port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=UTC", os.Getenv("DATABASE_URL"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), port)
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+		if err != nil {
+			panic("Database connection Error")
+		}
+
+		fmt.Println("Database Connection succed !")
+
+		if err := db.AutoMigrate(&models.User{}); err != nil {
+			panic("Error ro create the table")
+		}
+
+		return db
 	}
 
 	port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
@@ -34,4 +50,5 @@ func Connection() *gorm.DB {
 	}
 
 	return db
+
 }
